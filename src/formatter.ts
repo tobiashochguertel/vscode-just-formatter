729dae261a68089277f80c0aa037bfbda2ef6686
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2024-12-05 20:47:39 +0100

    first working version

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,15 +1,16 @@
-import * as vscode from 'vscode';
 import { exec } from "child_process";
+import * as vscode from 'vscode';
 import { documentToString, LoggerOutputType, minilogger, rangeToString } from "./utils/minilogger";
 
 export function formatDocument(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
     const log = minilogger(LoggerOutputType.OUTPUT_CHANNEL);
     log("Formatting Justfile...");
     log("vscode.TextDocument: ", documentToString(document));
+    const justFmtCommand = `just --fmt --unstable --justfile ${document.fileName}`;
     return new Promise((resolve, reject) => {
         try {
             const cwd = document.uri.fsPath.split('/').slice(0, -1).join('/');
-            exec("just --fmt --unstable --justfile", { cwd }, (err, stdout) => {
+            exec(justFmtCommand, { cwd }, (err, stdout) => {
                 if (err) {
                     if (err instanceof Error) {
                         log(`Error formatting Justfile: ${err.message}`);
@@ -23,7 +24,11 @@ export function formatDocument(document: vscode.TextDocument): Promise<vscode.Te
                 log('Formatting successful.');
                 if (!stdout) {
                     log('No output from just command.');
-                    return resolve([]);
+                    // Revert the file to load the changes from disk (in the context of VS Code, it actually means "revert to the version on disk".)
+                    vscode.commands.executeCommand("workbench.action.files.revert", document.uri).then(() => {
+                        // Return an empty array to indicate no text edits are needed
+                        return resolve([]);
+                    });
                 }
                 const fullRange = new vscode.Range(
                     document.positionAt(0),
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,54 +5,6 @@ import * as fs from "fs";
 import { execSync } from "child_process";
 import { suite } from "node:test";
 
-suite("Just Formatter Extension Tests", function () {
-	const loggerFilename = "../../logs/just-formatter.log"
-	const fixturesPath = path.join(__dirname, "../../test-fixtures");
-	const sampleFilePath = path.normalize(path.join(fixturesPath, `sample.justfile`));
-	const formattedSampleFilePath = path.normalize(path.join(fixturesPath, `/formatted.justfile`));
-
-	setup(async function () {
-		if (fs.existsSync(loggerFilename)) {
-			fs.unlinkSync(loggerFilename);
-		}
-		// Ensure the test fixture exists before running the tests.
-		if (!fs.existsSync(sampleFilePath)) {
-			throw new Error("Test fixture sample.justfile is missing.");
-		}
-		createFormattedSample(sampleFilePath, formattedSampleFilePath);
-	});
-
-	teardown(async function () {
-		// Cleanup: Remove any test output files if needed.
-		if (fs.existsSync(formattedSampleFilePath)) {
-			fs.unlinkSync(formattedSampleFilePath);
-		}
-	});
-
-	test("Extension is activated", async function () {
-		const extension = vscode.extensions.getExtension("TobiasHochguertel.just-formatter");
-		assert.ok(extension, "Extension should be found");
-		await extension?.activate();
-		const isActive = extension?.isActive;
-		assert.strictEqual(isActive, true, "Extension should be activated.");
-	});
-
-	test("Formatter produces expected output", async function () {
-		const document = await vscode.workspace.openTextDocument(sampleFilePath);
-		const editor = await vscode.window.showTextDocument(document);
-
-		// Invoke the formatting command programmatically.
-		await vscode.commands.executeCommand("editor.action.formatDocument");
-
-		const formattedContent = editor.document.getText();
-
-		const expectedFormattedContent = fs.readFileSync(formattedSampleFilePath, 'utf8');
-
-		// Check if the formatted content matches the expected content.
-		assert.strictEqual(formattedContent, expectedFormattedContent, "Formatted content should match `just --fmt` output.");
-	});
-});
-
 /**
  * Creates a formatted sample file by copying the original sample and applying Just formatting.
  * @param sample - Path to the original sample Justfile.
@@ -64,3 +16,75 @@ function createFormattedSample(sample: string, formatted: string) {
 		cwd: path.dirname(formatted),
 	}).toString();
 }
+
+const loggerFilename = path.join(__dirname, "../../logs/just-formatter.log");
+suite("Just Formatter Extension Tests", function () {
+	const workspacePath = path.join(__dirname, "../../test-workspace");
+	const fixturesPath = path.join(__dirname, "../../test-fixtures");
+	const sample = "sample.justfile"
+
+	const getSample = (filename: string): string => path.normalize(path.join(fixturesPath, filename));
+	const getFormattedSample = (filename: string): string => path.normalize(path.join(fixturesPath, `formatted.${filename}`));
+	const getFileInWorkspace = (filename: string): string => path.normalize(path.join(workspacePath, filename));
+
+	/**
+	 * Prepares the test environment by ensuring the sample Justfile exists and is copied to the workspace.
+	 * It also creates a formatted version of the sample for comparison.
+	 * @throws Error if the sample Justfile is missing in the fixtures directory.
+	 */
+	function prepareForTest() {
+		const samplePath = getSample(sample);
+		if (!fs.existsSync(samplePath)) {
+			throw new Error(`Test fixture "${path.basename(samplePath)}" is missing in the fixtures directory "${fixturesPath}".`);
+		}
+		fs.mkdirSync(workspacePath);
+		createFormattedSample(samplePath, getFormattedSample(sample));
+		fs.copyFileSync(samplePath, getFileInWorkspace(sample));
+	}
+
+	setup(async function () {
+		if (fs.existsSync(workspacePath)) {
+			console.log(`Cleaning up old workspace... ${workspacePath}`);
+			fs.rmSync(workspacePath, { recursive: true, force: true });
+		}
+		if (fs.existsSync(loggerFilename)) {
+			console.log(`Cleaning up old log file... ${loggerFilename}`);
+			fs.unlinkSync(loggerFilename);
+		}
+		const formattedSample = getFormattedSample(sample);
+		if (fs.existsSync(formattedSample)) {
+			// Cleanup: Remove any test output files if needed.
+			console.log(`Cleaning up old formatted file... ${formattedSample}`);
+			fs.unlinkSync(formattedSample);
+		}
+		prepareForTest();
+	});
+
+	teardown(async function () {
+
+	});
+
+	test("Extension is activated", async function () {
+		const extension = vscode.extensions.getExtension("TobiasHochguertel.just-formatter");
+		assert.ok(extension, "Extension should be found");
+		await extension?.activate();
+		const isActive = extension?.isActive;
+		assert.strictEqual(isActive, true, "Extension should be activated.");
+	});
+
+	test("Formatter produces expected output", async function () {
+		const document = await vscode.workspace.openTextDocument(getFileInWorkspace(sample));
+		const editor = await vscode.window.showTextDocument(document);
+
+		// Invoke the formatting command programmatically.
+		await vscode.commands.executeCommand("editor.action.formatDocument");
+
+		const formattedContent = editor.document.getText();
+
+		const expectedFormattedContent = fs.readFileSync(getFormattedSample(sample), 'utf8');
+
+		// Check if the formatted content matches the expected content.
+		assert.strictEqual(formattedContent, expectedFormattedContent, "Formatted content should match `just --fmt` output.");
+	});
+
+});
