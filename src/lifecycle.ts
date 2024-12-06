5bfbc2e056977cf6c229ac1fc2616222ff601a38
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2024-12-06 15:00:14 +0100

    Improve the presentation

---
+++ src/lifecycle.ts
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+
+// Log uninstallation
+console.log('Uninstalling just-formatter extension...');
+
+// No cleanup needed as we don't create any persistent files
+// This is just for logging purposes
+
+console.log('just-formatter extension uninstalled successfully.');
